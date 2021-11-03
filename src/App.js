35605535fbbb9844aabb59cc2e68b0aa5295beed
rmadan7 <rishi.madanraj@gmail.com>
2021-11-03 16:52:41 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I was here my guy</p>
+        <p>Bye there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was here my guy
+          Learn React
         </a>
       </header>
     </div>
